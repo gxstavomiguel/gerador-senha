0e158c239d6b9d80ce52f34f89963695242e2fb3
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-11-16 14:53:36 -0300

    first commit

---
+++ chatbot/script.ts
@@ -0,0 +1,9 @@
+var senhaGerada = '';
+const letras = ['a', 'b', 'c', 'd'];
+const numero = [1, 2, 3, 4];
+var numeroParaUsar: number;
+
+const valorAleatorio = Math.random();
+const valorAleatorio2 = Math.floor(10);
+console.log(valorAleatorio)
+
